0582e032745cbb351b3de677193eaa6ac0c9f99f
zhyan8 <815189404@qq.com>
2022-05-28 01:01:58 +0800

    install

--- host/src/main/java/com/zhyan8/host/utils/FileUtils.java
+++ host/src/main/java/com/zhyan8/host/utils/FileUtils.java
@@ -7,9 +7,9 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 public class FileUtils {
-    public static boolean copyFile(String oldPath$Name, String newPath$Name) {
+    public static boolean copyFile(String oldPath, String newPath) {
         try {
-            File oldFile = new File(oldPath$Name);
+            File oldFile = new File(oldPath);
             if (!oldFile.exists()) {
                 Log.e("--Method--", "copyFile:  oldFile not exist.");
                 return false;
@@ -21,8 +21,8 @@ public class FileUtils {
                 return false;
             }
 
-            FileInputStream fileInputStream = new FileInputStream(oldPath$Name);    //读入原文件
-            FileOutputStream fileOutputStream = new FileOutputStream(newPath$Name);
+            FileInputStream fileInputStream = new FileInputStream(oldPath);    //读入原文件
+            FileOutputStream fileOutputStream = new FileOutputStream(newPath);
             byte[] buffer = new byte[1024];
             int byteRead;
             //!=-1 也可以写成！=null,意思是读取的数据不为负数或者null就说明还没有读取完毕
@@ -32,65 +32,6 @@ public class FileUtils {
             fileInputStream.close();
             fileOutputStream.flush();
             fileOutputStream.close();
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    /**
-     * 复制文件夹及其中的文件
-     *
-     * @param oldPath String 原文件夹路径 如：data/user/0/com.test/files
-     * @param newPath String 复制后的路径 如：data/user/0/com.test/cache
-     * @return <code>true</code> if and only if the directory and files were copied;
-     * <code>false</code> otherwise
-     */
-    public boolean copyFolder(String oldPath, String newPath) {
-        try {
-            File newFile = new File(newPath);
-            if (!newFile.exists()) {
-                if (!newFile.mkdirs()) {
-                    Log.e("--Method--", "copyFolder: cannot create directory.");
-                    return false;
-                }
-            }
-            File oldFile = new File(oldPath);
-            String[] files = oldFile.list();
-            File temp;
-            for (String file : files) {
-                if (oldPath.endsWith(File.separator)) {
-                    temp = new File(oldPath + file);
-                } else {
-                    temp = new File(oldPath + File.separator + file);
-                }
-
-                if (temp.isDirectory()) {   //如果是子文件夹
-                    copyFolder(oldPath + "/" + file, newPath + "/" + file);
-                } else if (!temp.exists()) {
-                    Log.e("--Method--", "copyFolder:  oldFile not exist.");
-                    return false;
-                } else if (!temp.isFile()) {
-                    Log.e("--Method--", "copyFolder:  oldFile not file.");
-                    return false;
-                } else if (!temp.canRead()) {
-                    Log.e("--Method--", "copyFolder:  oldFile cannot read.");
-                    return false;
-                } else {
-                    FileInputStream fileInputStream = new FileInputStream(temp);
-                    FileOutputStream fileOutputStream = new FileOutputStream(newPath + "/" + temp.getName());
-                    byte[] buffer = new byte[1024];
-                    int byteRead;
-                    //!=-1 也可以写成！=null,意思是读取的数据不为负数或者null就说明还没有读取完毕
-                    while ((byteRead = fileInputStream.read(buffer)) != -1) {
-                        fileOutputStream.write(buffer, 0, byteRead);
-                    }
-                    fileInputStream.close();
-                    fileOutputStream.flush();
-                    fileOutputStream.close();
-                }
-            }
             return true;
         } catch (Exception e) {
             e.printStackTrace();
